bfb2508149a06b80eb6745cedfd7818d051488ce
Blervin <blervin121212@gmail.com>
2025-01-05 20:03:51 +0100

    make build

---
+++ index.56bbb5ac.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var n=t.target.closest(".list-item__link");n&&t.preventDefault(),e.src=n.href});
+//# sourceMappingURL=index.56bbb5ac.js.map
